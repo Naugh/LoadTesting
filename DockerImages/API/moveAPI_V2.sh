a144b34413b430571b6ac33ae179780af2e28e8b
Jorge <jorge_jrb90@hotmail.com>
2021-05-20 16:38:07 -0700

    API: Restoring API V1

---
+++ DockerImages/API/moveAPI_V2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#mv /home/naugh/GitHub/LoadTesting/LoadTestingAPI/target/LoadTestingAPI-0.0.1-SNAPSHOT.jar /home/naugh/GitHub/LoadTesting/DockerImages/API/LoadTestingAPI.jar
+cd ..
+cd ..
+mv LoadTestingAPIV2/target/LoadTestingAPI-0.0.1-SNAPSHOT.jar DockerImages/API/LoadTestingAPI.jar
+read -p "Press any key"
