ac95d82192fbeab0247ea1d9166c5e60a1ad290f
Jorge <j.rodriguezbarr@alumnos.urjc.es>
2022-07-10 20:56:42 +0200

    Cleaning master branch

--- LoadTestingAPIV2/src/main/java/main/JMeterResultsRepository.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JMeterResultsRepository extends JpaRepository<JMeterResults, Long>{
-
-}
--- LoadTestingAngular/src/app/j-meter-result.model.ts
+++
@@ -1,8 +0,0 @@
-import {Result} from "./result.model";
-
-export interface JMeterResult {
-    id? : number;
-    results? : Array<Result>;
-    systemDate? : String;
-    name : String;
-}
